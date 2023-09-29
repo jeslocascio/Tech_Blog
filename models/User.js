33bf398a02eeb62937b5141d25fb4f0fff15568e
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-09-29 13:21:43 -0400

    Completed User.js Model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrpyt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrpyt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrpyt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    },
+);
+
+module.exports = User;
